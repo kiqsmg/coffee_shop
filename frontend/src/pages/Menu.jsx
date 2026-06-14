372e663e3569948f8af24e1b014fd5a11f376808
gabriela-ssj <gabrielajesus755@gmail.com>
2026-06-14 19:15:39 -0300

    implementações de front-end

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -1,3 +1,19 @@
-// TODO: Menu page
-const Menu = () => <div>Menu</div>;
+import React from "react";
+
+function Menu() {
+  return (
+    <div className="menu-container">
+      <h1>Nosso Menu</h1>
+      <ul>
+        <li><strong>Cappuccino</strong> - R$ 12,00</li>
+        <li><strong>Expresso</strong> - R$ 8,00</li>
+        <li><strong>Matcha Latte</strong> - R$ 20,00</li>
+        <li><strong>Frappuccino</strong> - R$ 20,00</li>
+        <li><strong>Macchiato</strong> - R$ 18,00</li>
+        <li><strong>Soda Italiana</strong> - R$ 14,00</li>
+      </ul>
+    </div>
+  );
+}
+
 export default Menu;
