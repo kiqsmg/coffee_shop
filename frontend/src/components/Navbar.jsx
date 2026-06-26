67b19c3e99a637163c4ee031ee4dd2253582ab02
gabriela-ssj <gabrielajesus755@gmail.com>
2026-06-25 21:30:06 -0300

    ajustes front

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { FaUser, FaSearch, FaShoppingBag } from "react-icons/fa";
 
 function Navbar() {
-  const [cartCount, setCartCount] = useState(0); 
+  const [cartCount, setCartCount] = useState(0);
 
   return (
     <header className="navbar bg-dark text-white py-4 px-6 flex items-center justify-between">
@@ -14,8 +14,8 @@ function Navbar() {
         </h1>
       </div>
 
-      {/* Links */}
-      <nav className="nav-links flex space-x-6 font-body">
+      {/* Links centralizados */}
+      <nav className="nav-links flex-1 flex justify-center space-x-12 font-body">
         <Link to="/menu" className="hover:text-gray-300 transition-colors">
           MENU
         </Link>
@@ -32,7 +32,10 @@ function Navbar() {
         <Link to="/login" className="hover:text-gray-300 transition-colors">
           <FaUser />
         </Link>
-        <Link to="/sacola" className="hover:text-gray-300 transition-colors relative">
+        <Link
+          to="/sacola"
+          className="hover:text-gray-300 transition-colors relative"
+        >
           <FaShoppingBag />
           {cartCount > 0 && (
             <span className="absolute -top-2 -right-2 bg-white text-black text-xs rounded-full w-5 h-5 flex items-center justify-center font-mono">
